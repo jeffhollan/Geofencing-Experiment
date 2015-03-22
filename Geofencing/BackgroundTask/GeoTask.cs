c79781300625cca8db240d4962994f20e4408211
jeffhollan <jeffhollan@outlook.com>
2015-03-22 16:00:53 -0700

    Got PushBullet Working on the Background Task

--- Geofencing/BackgroundTask/GeoTask.cs
+++ Geofencing/BackgroundTask/GeoTask.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp.Portable;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -49,44 +50,49 @@ namespace Geofencing.Task
 
             return taskRegistered;
         }
-        public void Run(IBackgroundTaskInstance taskInstance)
+        async public void Run(IBackgroundTaskInstance taskInstance)
         {
+			BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
+
             var reports = GeofenceMonitor.Current.ReadReports();
 
             var report = reports.FirstOrDefault(r => (r.Geofence.Id == "Home1") && (r.NewState == GeofenceState.Entered));
 
             if (report != null)
-                ToastNotify("Home1 Entered", report.Geofence.Id);
+                await ToastNotify("Home1 Entered", report.Geofence.Id);
 
             report = reports.FirstOrDefault(r => (r.Geofence.Id == "Work1") && (r.NewState == GeofenceState.Entered));
 
             if (report != null)
-                ToastNotify("Work1 Entered", report.Geofence.Id);
+                await ToastNotify("Work1 Entered", report.Geofence.Id);
         
             report = reports.FirstOrDefault(r => (r.Geofence.Id == "Travel1") && (r.NewState == GeofenceState.Entered));
 
             if (report != null)
-                ToastNotify("Travel1 Entered", report.Geofence.Id);
+                await ToastNotify("Travel1 Entered", report.Geofence.Id);
 
             report = reports.FirstOrDefault(r => (r.Geofence.Id == "Work1") && (r.NewState == GeofenceState.Exited));
 
             if (report != null)
-                ToastNotify("Work1 Exited", report.Geofence.Id);
+                await ToastNotify("Work1 Exited", report.Geofence.Id);
 
             report = reports.FirstOrDefault(r => (r.Geofence.Id == "Travel1") && (r.NewState == GeofenceState.Exited));
 
             if (report != null)
-                ToastNotify("Travel1 Exited", report.Geofence.Id);
+                await ToastNotify("Travel1 Exited", report.Geofence.Id);
 
             report = reports.FirstOrDefault(r => (r.Geofence.Id == "Home1") && (r.NewState == GeofenceState.Exited));
 
             if (report != null)
-                ToastNotify("Home1 Exited", report.Geofence.Id);
+                await ToastNotify("Home1 Exited", report.Geofence.Id);
+
+			deferral.Complete();
 
         }
 
-        private void ToastNotify(string txt, string geoId)
+        async private System.Threading.Tasks.Task ToastNotify(string txt, string geoId)
         {
+			await PushBullet(txt, geoId);
             var toastXmlContent = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
             var txtNodes = toastXmlContent.GetElementsByTagName("text");
             txtNodes[0].AppendChild(toastXmlContent.CreateTextNode(txt));
@@ -96,5 +102,27 @@ namespace Geofencing.Task
             var toastNotifier = ToastNotificationManager.CreateToastNotifier();
             toastNotifier.Show(toast);
         }
+
+		async private System.Threading.Tasks.Task PushBullet(string title, string body)
+		{
+			var client = new RestClient("https://api.pushbullet.com/v2");
+			var request = new RestRequest("pushes");
+			request.Method = System.Net.Http.HttpMethod.Post;
+			request.AddParameter("type", "note");
+			request.AddParameter("title", title);
+			request.AddParameter("body", body);
+
+			request.AddHeader("Authorization", "Bearer VqxmF1tRVnFzGJpsg46AS19IHm5RJCM6");
+			try
+			{
+				var response = await client.Execute(request) as RestResponse;
+				Debug.WriteLine("Response: {0}", response.StatusCode.ToString());
+
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.ToString());
+			}
+		}
     }
 }
