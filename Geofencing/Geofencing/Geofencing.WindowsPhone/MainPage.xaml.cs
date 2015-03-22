9f056fb2c097c2d8a7074c1977b59b13ce99f52b
jeffhollan <jeffhollan@outlook.com>
2015-03-21 17:11:58 -0700

    LIve Trial 2, no pushbullet

--- Geofencing/Geofencing/Geofencing.WindowsPhone/MainPage.xaml.cs
+++ Geofencing/Geofencing/Geofencing.WindowsPhone/MainPage.xaml.cs
@@ -221,18 +221,24 @@ namespace Geofencing
 
 		private async void pushbullet_Click(object sender, RoutedEventArgs e)
 		{
-			var client = new RestClient("http://api.pushbullet.com/v2/");
-			var request = new RestRequest("pushes");
-			request.AddParameter("type", "note");
-			request.AddParameter("title", "Testing");
-			request.AddParameter("body", "This was sent from the WP app");
+			//var client = new RestClient("http://api.pushbullet.com/v2/");
+			//var request = new RestRequest("pushes");
+			//request.AddParameter("type", "note");
+			//request.AddParameter("title", "Testing");
+			//request.AddParameter("body", "This was sent from the WP app");
 
-			request.AddHeader("Authorization", string.Format("Bearer {0}", Push_API_KEY));
+			//request.AddHeader("Authorization", string.Format("Bearer {0}", Push_API_KEY));
+			//try
+			//{
+			//	RestResponse response = await client.Execute(request) as RestResponse;
+			//	Debug.WriteLine("Response: {0}", response.StatusCode.ToString());
 
-			RestResponse response = await client.Execute(request) as RestResponse;
-
-			//return response.Data;
-			Debug.WriteLine("Response: {0}", response.StatusCode.ToString());
+			//}
+			//catch (Exception ex)
+			//{
+			//	Debug.WriteLine(ex.ToString());
+			//}
+			////return response.Data;
 		}
 
 		
